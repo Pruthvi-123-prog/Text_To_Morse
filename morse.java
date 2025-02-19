603b6305de5c2ecc79f800af7ac8d7e7f9e55553
Pruthvi Suvarna K M <126673580+Pruthvi-123-prog@users.noreply.github.com>
2025-02-20 02:04:13 +0530

    Add files via upload

---
+++ morse.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class morse {
+    public static void main(String[] args) {
+        String[] morse = {
+            ".- ",    // A
+            "-... ",  // B
+            "-.-. ",  // C
+            "-.. ",   // D
+            ". ",     // E
+            "..-. ",  // F
+            "--. ",   // G
+            ".... ",  // H
+            ".. ",    // I
+            ".--- ",  // J
+            "-.- ",   // K
+            ".-.. ",  // L
+            "-- ",    // M
+            "-. ",    // N
+            "--- ",   // O
+            ".--. ",  // P
+            "--.- ",  // Q
+            ".-. ",   // R
+            "... ",   // S
+            "- ",     // T
+            "..- ",   // U
+            "...- ",  // V
+            ".-- ",   // W
+            "-..- ",  // X
+            "-.-- ",  // Y
+            "--.. "   // Z
+        };
+
+        System.out.println("Enter the text you want to convert to morse:");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        char[] morse_char = word.toUpperCase().toCharArray();
+        for(char i : morse_char){
+            if (i == ' ') {
+                System.out.print("  "); 
+            } else if (i >= 'A' && i <= 'Z') {
+                int morse_index = i - 'A';
+                System.out.print(morse[morse_index]);
+            }
+        }
+        scanner.close();
+    }        
+}
